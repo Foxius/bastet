884a3b659f33ca9e54a9b3e981747972fe9bea88
saikonohack <saintklovus@gmail.com>
2025-02-18 14:30:40 +0500

    hotfix

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 import dotenv
+
 dotenv.load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHATS = os.getenv("CHATS")
+CHATS = [int(chat_id) for chat_id in os.getenv("CHATS").strip("[]").split(",")]
 DATABASE_NAME = os.getenv("DATABASE_NAME")
-ADMIN_IDS = os.getenv("ADMIN_IDS")
+ADMIN_IDS = [int(admin_id) for admin_id in os.getenv("ADMIN_IDS").strip("[]").split(",")]
